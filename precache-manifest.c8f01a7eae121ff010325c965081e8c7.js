eefc10adef0eaf36bcf03a72de17d847c34dc4b0
KhushiN893 <khushi03.it@gmail.com>
2022-09-16 22:33:34 +0530

    Updates

---
+++ precache-manifest.c8f01a7eae121ff010325c965081e8c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "935e0cba8be07dce34da",
+    "url": "/NewsDose/static/css/main.41134591.chunk.css"
+  },
+  {
+    "revision": "935e0cba8be07dce34da",
+    "url": "/NewsDose/static/js/main.935e0cba.chunk.js"
+  },
+  {
+    "revision": "5c1e5c88ed5ad22945cc",
+    "url": "/NewsDose/static/js/1.5c1e5c88.chunk.js"
+  },
+  {
+    "revision": "39025e9385c0901bf6c1",
+    "url": "/NewsDose/static/js/runtime~main.39025e93.js"
+  },
+  {
+    "revision": "6f02b542e433cc375b2d5a0fdc231150",
+    "url": "/NewsDose/static/media/loading.6f02b542.gif"
+  },
+  {
+    "revision": "d7a4b492cd6f81c623b8dc56a7e9f6ee",
+    "url": "/NewsDose/static/media/error.d7a4b492.mp4"
+  },
+  {
+    "revision": "615e2e90f07590ea9befce9c9ac59302",
+    "url": "/NewsDose/index.html"
+  }
+];
